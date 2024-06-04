32681ccd9cba61ff116aa7adb0faf8c319a61155
ValerioViteritti <vviteritti@gmail.com>
2024-06-04 09:45:36 +0200

    Add comment in js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,18 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 // Chiedi all’utente il suo nome
 
 let nameUser = prompt("Inserisci il tuo nome");
+
+// chiedi il suo cognome
 let SurnameUser = prompt("Inserisci il tuo cognome");
+
+// chiedi il suo colore preferito
 let ColorUser = prompt("Inserisci il tuo colore preferito");
 
+// Risultato
 let passGenerator = nameUser + SurnameUser + ColorUser + "23";
 
+// risultato console
 console.log("La tua password generata è " + passGenerator);
 
+// Risultato in pagina
 document.getElementById("PassGenerator").innerHTML = "La tua password generata è " + passGenerator;
